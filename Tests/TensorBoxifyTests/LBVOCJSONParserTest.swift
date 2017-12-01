b05524f6f5085fd7489549f50c5a336a60762303
Dan Hushon <hushon@gmail.com>
2017-12-01 12:24:09 -0500

    Adding Test Cases..

--- Tests/TensorBoxifyTests/LBVOCJSONParserTest.swift
+++ Tests/TensorBoxifyTests/LBVOCJSONParserTest.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+
+import XCTest
+
+@testable import TensorBoxify
+
+class LBVOCJSONParserTest: XCTestCase {
+    
+    let file = ["lepage-170604-DGP-9916"]
+    let type = "json"
+    var vocs : VOCElementSet? = nil
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testJSONDecoder() {
+        // get url (check that bundle includes necessary JSON)
+        let jsonURL = Resource(name: file[0], type: type).url
+        try XCTAssertTrue((jsonURL.checkResourceIsReachable()))
+        print("JSONURL: \(String(describing: jsonURL)) is reachable")
+        
+        // parse json file identified
+        let parser = LBVOCJSONParser()
+        do {
+            let _ = try parser.decode(url: jsonURL)
+            let voc: VOCElementSet? = parser.getParsed()
+            XCTAssertNotNil(voc)
+            self.vocs = voc
+            let image = voc!.images[0]
+            XCTAssertTrue(image.objects.count == 3)
+        } catch {
+            print("Caught Error: \(error)")
+        }
+        print("JSONParse: successfully parsed objects")
+    }
+    
+    func testJSONEncoder() {
+        testJSONDecoder()
+        let jsonURL = Resource(name: file[0], type: type).url
+        do {
+            let tbp = LBVOCJSONParser()
+            try tbp.encode(url: jsonURL,voc: vocs)
+            print("JSONParser.encoder successfully encoded objects")
+        } catch {
+            print("VOCJSONDecoderTest.testJSONEncoder: \(error)")
+            XCTAssertFalse(true)
+        }
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func getVOCElementSet() -> VOCElementSet? {
+        return vocs
+    }
+    
+}
--- Tests/TensorBoxifyTests/Resource.swift
+++ Tests/TensorBoxifyTests/Resource.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class Resource {
+    static var resourcePath = "./Resources"
+    
+    let name: String
+    let type: String
+    
+    init(name: String, type: String) {
+        self.name = name
+        self.type = type
+    }
+    
+    var path: String {
+        guard let path: String = Bundle(for: Swift.type(of: self)).path(forResource: name, ofType: type) else {
+            let filename: String = type.isEmpty ? name : "\(name).\(type)"
+            return "\(Resource.resourcePath)/\(filename)"
+        }
+        return path
+    }
+    
+    var url: URL {
+        let url: URL? = Bundle(for: Swift.type(of: self)).url(forResource: name, withExtension: type)
+        if (url != nil) {
+            return url!
+        } else {
+            let filename: String = (type.isEmpty ? name : "\(name).\(type)")
+            return URL(string: "file:///\(Resource.resourcePath)/\(filename)")!
+        }
+    }
+    
+    var content: String? {
+        return try? String(contentsOfFile: path).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+    
+    var base64EncodedData: Data? {
+        guard let string = content, let data = Data(base64Encoded: string) else {
+            return nil
+        }
+        return data
+    }
+    
+}
