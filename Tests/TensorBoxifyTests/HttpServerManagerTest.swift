219fa3cc3706408f2bc172acfe2984b9269e2b8b
Dan Hushon <hushon@gmail.com>
2017-12-04 11:03:59 -0500

    Added Swifter TestCase and repackaging for SPM

---
+++ Tests/TensorBoxifyTests/HttpServerManagerTest.swift
@@ -0,0 +1,30 @@
+//
+//  HttpServerManagerTests.swift
+//  TensorBoxifyPackageDescription
+//
+//  Created by Dan Hushon on 12/4/17.
+//
+
+import Foundation
+import XCTest
+
+class HttpServerManagerTests: XCTestCase {
+    
+    static var allTests = [
+        ("startServer", testStartServer),
+        ]
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testStartServer() {
+        print("hello world")
+    }
+    
+}
